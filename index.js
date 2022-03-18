457b8ec2a70aa3d724b7da1adaecf8b90f8e5749
duyilemi <67197664+duyilemi@users.noreply.github.com>
2022-03-18 11:46:58 +0100

    deploy app to heroku

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuid } = require("uuid");
+
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+app.use("/peerjs", peerServer);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuid()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.param.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT);
